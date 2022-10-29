28e4fec993cb45930997bfa760124b6a9be38315
unknown <sadafzillani98@gmail.com>
2022-10-29 20:38:43 +0530

    provateroute

--- src/component/PrvateRoute/PrivareRoute.js
+++ src/component/PrvateRoute/PrivareRoute.js
@@ -1,18 +1,1 @@
-import { useContext } from "react";
-import { Navigate } from "react-router-dom";
-import { AuthContext } from "./../../Contexts/AuthProvider";
 
-const PrivateRoute = ({ children }) => {
-  const { user, loading } = useContext(AuthContext);
-
-  if (loading) {
-    return <button className="btn btn-square loading"></button>;
-  }
-  if (user && user.uid) {
-    return children;
-  }
-
-  return <Navigate to="/login"></Navigate>;
-};
-
-export default PrivateRoute;
